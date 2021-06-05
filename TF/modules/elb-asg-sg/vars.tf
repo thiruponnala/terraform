0ee21add632b7fbe62edb78bcb3dc343c499ee0d
thiruponnala <85402445+thiruponnala@users.noreply.github.com>
2021-06-05 21:13:32 +0530

    Add files via upload

---
+++ TF/modules/elb-asg-sg/vars.tf
@@ -0,0 +1,22 @@
+variable "v_it"{
+	default="t2.micro"
+}
+variable v_keyname{
+    
+}
+variable v_vpc_cidr{
+  
+}
+variable v_desire{
+}
+variable v_min{
+}
+variable v_max{
+}
+variable v_vpc_id{
+}
+variable v_sn1_ids{
+	type="list"
+}
+variable v_ami{
+}
---
+++ TF/modules/network-vpc/vars.tf
@@ -0,0 +1,11 @@
+variable "v_vpc_cidr"{
+   default="10.0.0.0/16"
+}
+variable "v_vpc_id"{
+}
+variable "v_sn1"{
+	type="list"
+}
+variable "v_tags"{
+   default="test"
+}
