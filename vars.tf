583002d3a6f6996572cc8e55140a72b45aa9c9ac
thiruponnala <85402445+thiruponnala@users.noreply.github.com>
2021-06-05 20:11:38 +0530

    Add files via upload

--- vars.tf
+++ vars.tf
@@ -1,22 +1,11 @@
-variable "v_it"{
-	default="t2.micro"
+variable "v_vpc_cidr"{
+   default="10.0.0.0/16"
 }
-variable v_keyname{
-    
+variable "v_vpc_id"{
 }
-variable v_vpc_cidr{
-  
-}
-variable v_desire{
-}
-variable v_min{
-}
-variable v_max{
-}
-variable v_vpc_id{
-}
-variable v_sn1_ids{
+variable "v_sn1"{
 	type="list"
 }
-variable v_ami{
+variable "v_tags"{
+   default="test"
 }
